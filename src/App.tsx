a2f48247e9b8afe197cdabbe5570c07038090e64
Jonathan Butler <jonathan.butler@getweave.com>
2022-03-24 15:57:00 -0700

    rename component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './styles.css';
 import { DocumentApiSolution } from './solutions/document-api/document-api-solution.component';
-import { ProjectDescription } from './project-description';
+import { ProjectDescription } from './project-description.component';
 import { ArraySolution } from './solutions/array/array-solution.component';
 import { HashTableSolution } from './solutions/hash-table/hash-table-solution.component';
 
