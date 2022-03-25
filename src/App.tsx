759d6ee1f6c9abb8d5ffa2bd154dc3933c9cf48d
Jonathan Butler <jonathan.butler@getweave.com>
2022-03-25 12:41:08 -0700

    clean up some imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './styles.css';
-import { DocumentApiSolution } from './solutions/document-api/document-api-solution.component';
 import { ProjectDescription } from './project-description.component';
-import { ArraySolution } from './solutions/array/array-solution.component';
-import { HashTableSolution } from './solutions/hash-table/hash-table-solution.component';
+import {
+  DocumentApiSolution,
+  ArraySolution,
+  HashTableSolution,
+} from './solutions/';
 
 export function App() {
   return (
---
+++ src/solutions/index.ts
@@ -0,0 +1,3 @@
+export * from './array/array-solution.component';
+export * from './document-api/document-api-solution.component';
+export * from './hash-table/hash-table-solution.component';
